c87cfde6a4b8bd66af4b849ac5578455bcdf61a5
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-12-02 11:46:59 -0500

    Tween pointer movement, begin workspace.

--- src/main.py
+++ src/main.py
@@ -79,13 +79,54 @@ class InventoryItem(arcade.sprite.Sprite):
         return ii
 
 
+TWEEN_RATE = 5
 class Pointer(arcade.sprite.Sprite):
     def __init__(self):
-        super().__init__(
-
-        )
+        super().__init__()
         self.append_texture(TEXTURES['pointer'])
         self.set_texture(0)
+
+        self._tween = {}
+        self._tween_progress = 1
+
+    def update(self, dt):
+        if self._tween_progress == 1:
+            return
+        self._tween_progress = min(1, self._tween_progress + dt * TWEEN_RATE)
+        for key, (start, end) in self._tween.items():
+            setattr(self, key, (end - start) * self._tween_progress + start)
+        # need to force rescale :\
+        if 'scale' in self._tween:
+            self.set_texture(0)
+        if self._tween_progress == 1:
+            self._tween = {}
+
+    def _set_tween(self, **properties):
+        self._tween = {
+            key: (getattr(self, key), value)
+            for key, value in properties.items()
+        }
+        self._tween_progress = 0
+
+    def move_left(self):
+        if self._tween:
+            return
+        self._set_tween(left=self.left - 105)
+
+    def move_right(self):
+        if self._tween:
+            return
+        self._set_tween(left=self.left + 105)
+
+    def move_up(self):
+        if self._tween:
+            return
+        self._set_tween(top=self.top + 105)
+
+    def move_down(self):
+        if self._tween:
+            return
+        self._set_tween(top=self.top - 105)
 
 
 class InventoryScreen:
@@ -93,13 +134,23 @@ class InventoryScreen:
         offset_x = 55
         offset_y = 55
         self.tiles = {
-            (x, y): InventoryTile(
+            ('inventory', x, y): InventoryTile(
                 x=x * (TILE_SIZE + TILE_GAP) + offset_x,
                 y=y * (TILE_SIZE + TILE_GAP) + offset_y,
             )
             for x in range(5)
             for y in range(4)
         }
+        workspace_tile_size = 50
+        self.tiles.update({
+            ('workspace', x, y): InventoryTile(
+                x=x * (workspace_tile_size + 2.5) + 700,
+                y=y * (workspace_tile_size + 2.5) + 32,
+                size=workspace_tile_size,
+            )
+            for x in range(5)
+            for y in range(10)
+        })
         self.items = [
             InventoryItem.get_grapple_hook(105 * 1.5, 105 * 1.5),
             InventoryItem.get_mushroom(105 * 3.5, 105 * 1.5),
@@ -115,15 +166,18 @@ class InventoryScreen:
 
         self.pointer.draw()
 
+    def update(self, dt):
+        self.pointer.update(dt)
+
     def handle_action(self, action):
         if action == PlayerAction.up:
-            self.pointer.top += 105
+            self.pointer.move_up()
         elif action == PlayerAction.down:
-            self.pointer.top -= 105
+            self.pointer.move_down()
         elif action == PlayerAction.right:
-            self.pointer.left += 105
+            self.pointer.move_right()
         elif action == PlayerAction.left:
-            self.pointer.left -= 105
+            self.pointer.move_left()
 
 
 class PlayerAction(enum.Enum):
@@ -131,13 +185,20 @@ class PlayerAction(enum.Enum):
     down = 'down'
     left = 'left'
     up = 'up'
+    select = 'select'
 
 
 key_config = {
     arcade.key.RIGHT: PlayerAction.right,
     arcade.key.LEFT: PlayerAction.left,
     arcade.key.UP: PlayerAction.up,
-    arcade.key.DOWN: PlayerAction.down
+    arcade.key.DOWN: PlayerAction.down,
+    arcade.key.ENTER: PlayerAction.select,
+    arcade.key.D: PlayerAction.right,
+    arcade.key.A: PlayerAction.left,
+    arcade.key.W: PlayerAction.up,
+    arcade.key.S: PlayerAction.down,
+    arcade.key.SPACE: PlayerAction.select,
 }
 
 
@@ -155,6 +216,7 @@ class MyApplication(arcade.Window):
 
     def update(self, dt):
         """ Move everything """
+        self.ui.update(dt)
 
     def on_key_press(self, symbol, modifier):
         if (symbol, modifier) == (arcade.key.Q, arcade.key.MOD_COMMAND):
